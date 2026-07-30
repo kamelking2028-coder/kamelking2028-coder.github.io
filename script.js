3eaa3d159daf38a50c02b1970b515accc28d9cb5
KML M <kamelking2028@gmail.com>
2026-07-30 10:09:02 +0200

    Update print statement from 'Hello' to 'Goodbye'

--- script.js
+++ script.js
@@ -482,7 +482,7 @@ function renderRadios() {
       
             if (radio.stationuuid === "bassatine-manuel") {
                 const frame = document.getElementById("bassatineFrame");
-                frame.src = "https://www.radioquran.tn/?utm_source=tuninga"; 
+                frame.src = "http://www.tunisie-radio.com/player/17951";
                 document.getElementById("bassatineContainer").style.display = "block";
                 return;
             }
