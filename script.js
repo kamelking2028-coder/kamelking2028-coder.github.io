273294a158e810a8215268dfd2283a55128aac66
KML M <kamelking2028@gmail.com>
2026-08-09 11:09:47 +0200

    Update script.js

--- script.js
+++ script.js
@@ -387,6 +387,18 @@ async function fetchRadios(codePays) {
             }
 
             allRadiosCache = radios;
+     // Ajout Coordonnées geo. Monastir
+            if (codePays === "TN") {
+                allRadiosCache.push({
+                    name: "Radio Monastir",
+                    url: "Radio Monastir",
+                    countrycode: "TN",
+                    geo_lat:35.7779,
+                    geo_long:10.8261,
+                    tags: "arabic,community",
+                    stationuuid: "Monastir-manuel"
+                });
+            }      
 
      // Ajout manuel Arabel
             if (codePays === "BE") {
