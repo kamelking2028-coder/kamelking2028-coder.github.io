69664761e16c6cbc5853864451daaf27399cce84
KML M <kamelking2028@gmail.com>
2026-08-09 11:18:19 +0200

    Update print statement from 'Hello' to 'Goodbye'

--- script.js
+++ script.js
@@ -387,21 +387,6 @@ async function fetchRadios(codePays) {
             }
 
             allRadiosCache = radios;
-     // Ajout Coordonnées geo. Monastir
-             if (codePays === "TN") {
-                 allRadiosCache.push({
-                     name: "Radio Monastir",
-                     url: "monastir",
-                     favicon: "icons/monastir.png",
-                     countrycode: "TN",
-                     geo_lat: 35.7779,
-                     geo_long: 10.8261,
-                     tags: "arabic,community",
-                     stationuuid: "Monastir-manuel"
-                  });
-             }
-
-
      // Ajout manuel Arabel
             if (codePays === "BE") {
                 allRadiosCache.push({
