3e2f2ff5e616a8f05c7ebbe2f2c7072df93ea9e0
KML M <kamelking2028@gmail.com>
2026-09-02 21:35:21 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,9 +1,9 @@
 self.options = {
-    "domain": "5gvci.com",
-    "zoneId": 11661033
+    "domain": "3nbf4.com",
+    "zoneId": 11327421
 }
 self.lary = ""
-importScripts('https://5gvci.com/act/files/service-worker.min.js?r=sw')
+importScripts('https://3nbf4.com/act/files/service-worker.min.js?r=sw')
 
 
 
